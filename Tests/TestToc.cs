9ce04aebdca7b469858526a649c446232da25d6b
ryouji <helloryouji@outlook.com>
2023-05-28 07:28:49 +0800

    add a test unit for Toc.AddTocElemFromPageElem function

--- Tests/TestToc.cs
+++ Tests/TestToc.cs
@@ -189,4 +189,78 @@ public class TestToc
                       toc.TocElemList[0].Children[2].Title == "page6";
         Assert.IsTrue(isTrue);
     }
+
+    [Test]
+    public void GenerateTocFromPageList()
+    {
+        // PageList Struct:
+        // page1
+        // |—— page2
+        // |—— page3
+        // |   |—— page4
+        // |   |—— page5
+        // |—— page6
+        // page7
+        // |—— page8
+        // |—— page9
+        // |   |—— page10
+        // |   |—— page11
+        // |—— page12
+        PageList pageList = new PageList();
+        PageElement page1 = new PageElement(1,"page1");
+        PageElement page2 = new PageElement(2,"page2");
+        PageElement page3 = new PageElement(2,"page3");
+        PageElement page4 = new PageElement(3,"page4");
+        PageElement page5 = new PageElement(3,"page5");
+        PageElement page6 = new PageElement(2,"page6");
+        PageElement page7 = new PageElement(1,"page7");
+        PageElement page8 = new PageElement(2,"page8");
+        PageElement page9 = new PageElement(2,"page9");
+        PageElement page10 = new PageElement(3,"page10");
+        PageElement page11 = new PageElement(3,"page11");
+        PageElement page12 = new PageElement(2,"page12");
+
+        pageList.AddPageElem(page1,2);
+        pageList.AddPageElem(page2,2);
+        pageList.AddPageElem(page3,2);
+        pageList.AddPageElem(page4,2);
+        pageList.AddPageElem(page5,2);
+        pageList.AddPageElem(page6,2);
+        pageList.AddPageElem(page7,2);
+        pageList.AddPageElem(page8,2);
+        pageList.AddPageElem(page9,2);
+        pageList.AddPageElem(page10,2);
+        pageList.AddPageElem(page11,2);
+        pageList.AddPageElem(page12,2);
+
+        Toc toc = new Toc();
+        toc.GenerateTocFromPageList(pageList,2);
+        
+        // Toc Struct
+        // page1
+        // |—— page2
+        // |—— page3
+        // |   |—— page4
+        // |   |—— page5
+        // |—— page6
+        // page7
+        // |—— page8
+        // |—— page9
+        // |   |—— page10
+        // |   |—— page11
+        // |—— page12
+        bool isTrue = toc.TocElemList[0].Title == "page1" &&
+                      toc.TocElemList[0].Children[0].Title == "page2" &&
+                      toc.TocElemList[0].Children[1].Title == "page3" &&
+                      toc.TocElemList[0].Children[1].Children[0].Title == "page4" &&
+                      toc.TocElemList[0].Children[1].Children[1].Title == "page5" &&
+                      toc.TocElemList[0].Children[2].Title == "page6" &&
+                      toc.TocElemList[1].Title == "page7" &&
+                      toc.TocElemList[1].Children[0].Title == "page8" &&
+                      toc.TocElemList[1].Children[1].Title == "page9" &&
+                      toc.TocElemList[1].Children[1].Children[0].Title == "page10" &&
+                      toc.TocElemList[1].Children[1].Children[1].Title == "page11" &&
+                      toc.TocElemList[1].Children[2].Title == "page12";
+        Assert.IsTrue(isTrue);
+    }
 }
